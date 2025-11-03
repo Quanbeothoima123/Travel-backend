2c6ed4f52bd6832cf54b6f6132efb459e929b84c
Quanbeothoima123 <phimanhh85@gmail.com>
2025-11-03 16:40:30 +0700

    Cập nhật controller new-category đế lấy tất cả danh mục tin tức

--- api/v1/controllers/client/new-category.controller.js
+++ api/v1/controllers/client/new-category.controller.js
@@ -1,4 +1,5 @@
 const NewsCategory = require("../../models/new-category.model");
+const buildTree = require("../../../../helpers/buildTree");
 // [GET] /api/v1/get-news-category-by-slug
 exports.getNewsCategoryBySlug = async (req, res) => {
   try {
@@ -10,6 +11,25 @@ exports.getNewsCategoryBySlug = async (req, res) => {
       data: categoryDoc,
     });
   } catch (error) {
+    res.status(500).json({ message: "Lỗi lấy danh mục", error: error.message });
+  }
+};
+
+// [GET] /api/v1/news-categories/getAll
+module.exports.getAllNewCategories = async (req, res) => {
+  try {
+    const { tree } = req.query;
+    const filter = { deleted: false, active: true };
+
+    const newCategories = await NewsCategory.find(filter).sort({
+      createdAt: -1,
+    });
+    if (tree === "true") {
+      return res.json(buildTree(newCategories));
+    }
+    return res.json(newCategories);
+  } catch (error) {
+    console.error("Lỗi lấy danh mục tin tức:", error);
     res.status(500).json({ message: "Lỗi lấy danh mục", error: error.message });
   }
 };
