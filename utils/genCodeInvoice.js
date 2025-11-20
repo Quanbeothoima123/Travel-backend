a828e652a0faddc77f12a883d5169eafda474885
Quanbeothoima123 <phimanhh85@gmail.com>
2025-11-20 15:54:10 +0700

    Cập nhật hàm genCodeInvoice,js trong utils

--- utils/genCodeInvoice.js
+++ utils/genCodeInvoice.js
@@ -1,4 +1,7 @@
 const generateInvoiceCode = () => {
-  return "QQ-" + Date.now();
+  const prefix = "CASH-";
+  const timestamp = Date.now();
+  const random = Math.floor(Math.random() * 1000);
+  return `${prefix}${timestamp}${random}`;
 };
 module.exports = generateInvoiceCode;
