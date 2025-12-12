1d85ddfb5867943e9f5129f5b83faab6d68ac629
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-12 22:38:18 +0700

    Cập nhật checkRole cho gallery.route.js

--- api/v1/routes/admin/gallery.route.js
+++ api/v1/routes/admin/gallery.route.js
@@ -1,23 +1,59 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/gallery.controller");
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
+router.get(
+  "/manager",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.index
+);
 
-router.get("/manager", controller.index);
+router.patch(
+  "/toggle-active/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.toggleActive
+);
 
-router.patch("/toggle-active/:id", controller.toggleActive);
+router.post(
+  "/create",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.createGallery
+);
 
-router.post("/create", controller.createGallery);
+router.post(
+  "/generate-tags",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.generateTags
+);
 
-router.post("/generate-tags", controller.generateTags);
+router.get(
+  "/getAll",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getAllGalleries
+);
 
-router.get("/getAll", controller.getAllGalleries);
+router.get(
+  "/detail/:id",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getGalleryById
+);
 
-router.get("/detail/:id", controller.getGalleryById);
+router.get(
+  "/infoToEdit/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.getGalleryForEdit
+);
 
-router.get("/infoToEdit/:id", controller.getGalleryForEdit);
+router.patch(
+  "/update/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.updateGallery
+);
 
-router.patch("/update/:id", controller.updateGallery);
-
-router.delete("/delete/:id", controller.deleteGallery);
+router.delete(
+  "/delete/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.deleteGallery
+);
 
 module.exports = router;
