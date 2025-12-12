3dc41b1458ff2361b96b6cb42616b02a8341063d
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-13 01:03:00 +0700

    Cập nhật checkRole cho support.route.js

--- api/v1/routes/admin/support.route.js
+++ api/v1/routes/admin/support.route.js
@@ -2,10 +2,19 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/support.controller");
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
 // GET /api/v1/admin/support - Get all conversations
-router.get("/", controller.getAllConversations);
+router.get(
+  "/",
+  checkRole(["super-admin", "manager", "staff"]),
+  controller.getAllConversations
+);
 
 // GET /api/v1/admin/support/:conversationId - Get conversation detail
-router.get("/:conversationId", controller.getConversationDetailAdmin);
+router.get(
+  "/:conversationId",
+  checkRole(["super-admin", "manager", "staff"]),
+  controller.getConversationDetailAdmin
+);
 
 module.exports = router;
