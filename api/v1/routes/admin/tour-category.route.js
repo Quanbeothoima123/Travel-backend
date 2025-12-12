a432f40fdd73099bd6c0f520568f0082cb61433e
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-12 23:51:11 +0700

    cập nhật checkRole cho tour-category

--- api/v1/routes/admin/tour-category.route.js
+++ api/v1/routes/admin/tour-category.route.js
@@ -2,21 +2,52 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/tour-category.controller");
 const tourCategoryValidate = require("../../../../validates/admin/tour-category.validate");
-router.get("/get-all-category", controller.getAllCategories);
-router.get("/recent", controller.getRecentCategories);
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
+router.get(
+  "/get-all-category",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getAllCategories
+);
+router.get(
+  "/recent",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getRecentCategories
+);
 router.post(
   "/create",
+  checkRole(["super-admin", "manager"]),
   tourCategoryValidate.validateCreateCategory,
   controller.createCategory
 );
-router.get("/detail/:id", controller.getCategoryById);
+router.get(
+  "/detail/:id",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getCategoryById
+);
 router.patch(
   "/update/:id",
+  checkRole(["super-admin", "manager"]),
   tourCategoryValidate.validateUpdateCategory,
   controller.updateCategory
 );
-router.get("/delete-info/:id", controller.getDeleteCategoryInfo);
-router.delete("/delete/:id", controller.deleteCategory);
-router.get("/last-updated", controller.getLatestUpdatedCategory);
-router.get("/last-created", controller.getLatestCreatedCategory);
+router.get(
+  "/delete-info/:id",
+  checkRole(["super-admin", "manager"]),
+  controller.getDeleteCategoryInfo
+);
+router.delete(
+  "/delete/:id",
+  checkRole(["super-admin", "manager"]),
+  controller.deleteCategory
+);
+router.get(
+  "/last-updated",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getLatestUpdatedCategory
+);
+router.get(
+  "/last-created",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getLatestCreatedCategory
+);
 module.exports = router;
