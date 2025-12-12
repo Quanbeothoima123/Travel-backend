ed572da065c89cb228b9120a99cb3b8b9e931c5e
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-12 21:59:36 +0700

    Cập nhật checkRole cho dashboard.route.js

--- api/v1/routes/admin/dashboard.route.js
+++ api/v1/routes/admin/dashboard.route.js
@@ -1,14 +1,51 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/dashboard.controller");
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
 // Trong file routes của bạn
-router.get("/overview", controller.getOverview);
-router.get("/payment-methods", controller.getPaymentMethods);
-router.get("/top-tours", controller.getTopTours);
-router.get("/top-customers", controller.getTopCustomers);
-router.get("/user-analytics", controller.getUserAnalytics);
-router.get("/latest-news", controller.getLatestNews);
-router.get("/latest-tours", controller.getLatestTours);
-router.get("/recent-activities", controller.getRecentActivities);
-router.get("/revenue-metrics", controller.getRevenueMetrics);
+router.get(
+  "/overview",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getOverview
+);
+router.get(
+  "/payment-methods",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getPaymentMethods
+);
+router.get(
+  "/top-tours",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getTopTours
+);
+router.get(
+  "/top-customers",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getTopCustomers
+);
+router.get(
+  "/user-analytics",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getUserAnalytics
+);
+router.get(
+  "/latest-news",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getLatestNews
+);
+router.get(
+  "/latest-tours",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getLatestTours
+);
+router.get(
+  "/recent-activities",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getRecentActivities
+);
+router.get(
+  "/revenue-metrics",
+  checkRole(["super-admin", "manager", "viewer"]),
+  controller.getRevenueMetrics
+);
 module.exports = router;
