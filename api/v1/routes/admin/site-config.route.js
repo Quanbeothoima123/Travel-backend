cf951092e490c11f7428ddea6b73486733c2cb6a
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-12 22:43:41 +0700

    cập nhật checkRole cho site-config.route.js

--- api/v1/routes/admin/site-config.route.js
+++ api/v1/routes/admin/site-config.route.js
@@ -2,24 +2,64 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/site-config.controller");
-
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
 // Lấy cấu hình chung
-router.get("/", controller.get);
+router.get("/", checkRole(["super-admin", "manager"]), controller.get);
 
 // Tạo mới cấu hình (chỉ dùng lần đầu)
-router.post("/create", controller.create);
+router.post(
+  "/create",
+  checkRole(["super-admin", "manager"]),
+  controller.create
+);
 
 // Cập nhật toàn bộ
-router.patch("/update", controller.update);
+router.patch(
+  "/update",
+  checkRole(["super-admin", "manager"]),
+  controller.update
+);
 
 // Cập nhật từng phần
-router.patch("/company-info", controller.updateCompanyInfo);
-router.patch("/branches", controller.updateBranches);
-router.patch("/branding", controller.updateBranding);
-router.patch("/social-media", controller.updateSocialMedia);
-router.patch("/contact-floating", controller.updateContactFloating);
-router.patch("/newsletter", controller.updateNewsletter);
-router.patch("/seo", controller.updateSEO);
-router.patch("/settings", controller.updateSettings);
+router.patch(
+  "/company-info",
+  checkRole(["super-admin", "manager"]),
+  controller.updateCompanyInfo
+);
+router.patch(
+  "/branches",
+  checkRole(["super-admin", "manager"]),
+  controller.updateBranches
+);
+router.patch(
+  "/branding",
+  checkRole(["super-admin", "manager"]),
+  controller.updateBranding
+);
+router.patch(
+  "/social-media",
+  checkRole(["super-admin", "manager"]),
+  controller.updateSocialMedia
+);
+router.patch(
+  "/contact-floating",
+  checkRole(["super-admin", "manager"]),
+  controller.updateContactFloating
+);
+router.patch(
+  "/newsletter",
+  checkRole(["super-admin", "manager"]),
+  controller.updateNewsletter
+);
+router.patch(
+  "/seo",
+  checkRole(["super-admin", "manager"]),
+  controller.updateSEO
+);
+router.patch(
+  "/settings",
+  checkRole(["super-admin", "manager"]),
+  controller.updateSettings
+);
 
 module.exports = router;
