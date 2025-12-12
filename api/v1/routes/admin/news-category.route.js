a4d2623fac2299dde3017e1e987adff51e8cf60e
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-12 21:28:04 +0700

    Cập nhật checkRole cho news-category.routes.js

--- api/v1/routes/admin/news-category.route.js
+++ api/v1/routes/admin/news-category.route.js
@@ -2,22 +2,49 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/new-category.controller");
 const newsCategoryValidate = require("../../../../validates/admin/news-category.validate");
-router.get("/getAll", controller.getAllNewCategories);
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
+router.get(
+  "/getAll",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getAllNewCategories
+);
 router.post(
   "/create",
+  checkRole(["super-admin", "manager"]),
   newsCategoryValidate.validateCreateCategory,
   controller.createCategory
 );
-router.get("/detail/:id", controller.getCategoryById);
+router.get(
+  "/detail/:id",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getCategoryById
+);
 
 router.patch(
   "/update/:id",
+  checkRole(["super-admin", "manager"]),
   newsCategoryValidate.validateUpdateCategory,
   controller.updateCategory
 );
 router.get("/get-by-id/:id", controller.getById);
-router.get("/delete-info/:id", controller.getDeleteCategoryInfo);
-router.delete("/delete/:id", controller.deleteCategory);
-router.get("/last-updated", controller.getLatestUpdatedCategory);
-router.get("/last-created", controller.getLatestCreatedCategory);
+router.get(
+  "/delete-info/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.getDeleteCategoryInfo
+);
+router.delete(
+  "/delete/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.deleteCategory
+);
+router.get(
+  "/last-updated",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getLatestUpdatedCategory
+);
+router.get(
+  "/last-created",
+  checkRole(["super-admin", "manager", "staff", "writter", "viewer"]),
+  controller.getLatestCreatedCategory
+);
 module.exports = router;
