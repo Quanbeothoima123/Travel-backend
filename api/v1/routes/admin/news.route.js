b377ac08d1b1db0b393f6403667e365827f0b009
Quanbeothoima123 <phimanhh85@gmail.com>
2025-12-12 21:19:51 +0700

    Cập nhật checkRole cho các route của news

--- api/v1/routes/admin/news.route.js
+++ api/v1/routes/admin/news.route.js
@@ -1,18 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/news.controller");
-
-router.post("/create", controller.create);
+const { checkRole } = require("../../../../middlewares/admin/authAdmin");
+router.post(
+  "/create",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.create
+);
 router.get("/published", controller.getPublishedNews);
 router.get("/manager", controller.getNewsList);
 router.get("/detail/:id", controller.getNewsById);
-router.delete("/delete/:id", controller.deleteNews);
+router.delete(
+  "/delete/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.deleteNews
+);
 router.get("/news-categories", controller.getNewsCategories);
 router.get("/authors", controller.getAuthors);
-router.get("/get-data-for-edit/:id", controller.getNewsForEdit);
-router.patch("/edit/:id", controller.updateNews);
+router.get(
+  "/get-data-for-edit/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.getNewsForEdit
+);
+router.patch(
+  "/edit/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.updateNews
+);
 router.get("/check-slug/:id", controller.checkSlugAvailability);
 router.get("/detail/:id", controller.getNewsDetail);
-router.get("/update-status/:id", controller.updateNewsStatus);
-router.get("/update-engagement/:id", controller.updateEngagement);
+router.get(
+  "/update-status/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.updateNewsStatus
+);
+router.get(
+  "/update-engagement/:id",
+  checkRole(["super-admin", "manager", "writter"]),
+  controller.updateEngagement
+);
 module.exports = router;
